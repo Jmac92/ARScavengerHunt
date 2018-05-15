9eb2c7047daaf1f5bd7e1767a82f518d9e1bda14
100862367 <38548638+100862367@users.noreply.github.com>
2018-05-15 16:47:26 +1000

    FinderKeeper
    Prototype_Ang

---
+++ FinderKeeper/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour 
+{
+	public void LoadScene(string sceneName)
+	{
+		if (sceneName == null)
+			Debug.Log("<color=orange>"+gameObject.name+": No Scene Name Was given for LoadScene function!</color>");
+		SceneManager.LoadScene(sceneName); //load a scene
+	}
+}
---
+++ FinderKeeper/Assets/Scripts/Score.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+    public Text ScoreText;
+    private int score = 0;
+    private string scoreOutput = "Score = ";
+
+	// Use this for initialization
+	void Start () {
+        score = 0;
+        ScoreText.text = scoreOutput + score;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+	}
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "High")
+
+        {
+            score += 10;
+            ScoreText.text = scoreOutput + score;
+            Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.tag == "Mid")
+
+        {
+            score += 5;
+            ScoreText.text = scoreOutput + score;
+            Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.tag == "Low")
+
+        {
+            score += 2;
+            ScoreText.text = scoreOutput + score;
+            Destroy(collision.gameObject);
+        }
+
+    }
+}
+
