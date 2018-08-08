4bbbcb77fca65028e4787ded1703de310ed14d44
Justin <test@Tests-MacBook-Pro.local>
2018-08-09 02:04:38 +1000

    Added Orientation Button and Script
    Switches between isometric and third-person camera views when the button is pressed. Working as intended

---
+++ FinderKeeper/Assets/Scripts/CameraSwap.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwap : MonoBehaviour
+ {
+     public Camera IsometricCamera, ThirdPersonCamera;
+     public bool swapFlag = false;
+
+     public void Swap()
+     {
+         swapFlag = !swapFlag;
+         IsometricCamera.gameObject.SetActive(swapFlag);
+         ThirdPersonCamera.gameObject.SetActive(!swapFlag);
+     }
+ }
