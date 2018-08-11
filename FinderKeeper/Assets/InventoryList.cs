fcfb2a2d2fb23938783474ed349ce3d061d443d6
100862367 <38548638+100862367@users.noreply.github.com>
2018-08-11 17:48:47 +1000

    Item list and static items on map
    Items will be added to the item list whenever the "collect" button is clicked. Some items have been placed on the map around my house

---
+++ FinderKeeper/Assets/InventoryList.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryList : MonoBehaviour {
+    public GameObject inventoryList;
+    public Button openInventoryList;
+
+	// Use this for initialization
+	void Start () {
+        if (inventoryList != null)
+            inventoryList.SetActive(false);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Button button = openInventoryList.GetComponent<Button>();
+        button.onClick.AddListener(OpenInventoryList);
+	}
+
+    void OpenInventoryList()
+    {
+        inventoryList.SetActive(true);
+    }
+}
