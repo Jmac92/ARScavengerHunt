51542e66b2e9d62183badf2cd6d3754b07e50bbd
100862367 <38548638+100862367@users.noreply.github.com>
2018-08-17 01:54:10 +1000

    item_list + static_item_on_map + items_interact_with_touch
    Items can now be interacted through touching instead of collision which will open up the collection panel and add it into the inventory list. Static Items can be placed on the map by expanding the list.

--- FinderKeeper/Assets/Scripts/Collect.cs
+++ FinderKeeper/Assets/Scripts/Collect.cs
@@ -8,11 +8,14 @@ public class Collect : MonoBehaviour {
 
     public Text scoreText;
     public GameObject collectionPanel = null;
+    public GameObject InventoryList;
     public Button collectionButton;
-    
+
+    private Inventory _inventory;
     private int _score = 0;
     private string _scoreOutput = "Score = ";
     private GameObject _collectedItem = null;
+
 
     public void ShowCollectionPanel() {
         if (collectionPanel != null) {
@@ -28,14 +31,14 @@ public class Collect : MonoBehaviour {
         }
     }
 
-    void OnCollisionEnter(Collision collision)
+  /*  void OnCollisionEnter(Collision collision)
     {
         _collectedItem = collision.gameObject;
         ShowCollectionPanel();
-    }
+    }*/
 
     // Use this for initialization
-    void Start () {
+    void Awake () {
         HideCollectionPanel();
         _score = 0;
         scoreText.text = _scoreOutput + _score;
@@ -47,9 +50,11 @@ public class Collect : MonoBehaviour {
     void CollectionOnClick() {
         if (_collectedItem.tag == "High")
 
-        {
+        {            
             _score += 10;
             scoreText.text = _scoreOutput + _score;
+            _inventory = InventoryList.GetComponent<Inventory>();
+            _inventory.Add("High");
             Destroy(_collectedItem);
         }
         if (_collectedItem.tag == "Mid")
@@ -57,6 +62,8 @@ public class Collect : MonoBehaviour {
         {
             _score += 5;
             scoreText.text = _scoreOutput + _score;
+            _inventory = InventoryList.GetComponent<Inventory>();
+            _inventory.Add("Mid");
             Destroy(_collectedItem);
         }
         if (_collectedItem.tag == "Low")
@@ -64,15 +71,25 @@ public class Collect : MonoBehaviour {
         {
             _score += 2;
             scoreText.text = _scoreOutput + _score;
+            _inventory = InventoryList.GetComponent<Inventory>();
+            _inventory.Add("Low");
             Destroy(_collectedItem);
         }
         HideCollectionPanel();
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    
+    private void Update()
+    {
+        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit raycastHit;
+            if (Physics.Raycast(raycast, out raycastHit))
+            {
+                if (raycastHit.collider.tag == "High" || raycastHit.collider.tag == "Mid" || raycastHit.collider.tag == "Low") {
+                    _collectedItem = raycastHit.transform.gameObject;
+                    ShowCollectionPanel();
+                }
+            }
+        }
+    }
 }
