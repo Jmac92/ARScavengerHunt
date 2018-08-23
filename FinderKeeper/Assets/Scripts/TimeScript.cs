3596b5f0f5130bdcb609ba94206b60409b785269
100862367 <38548638+100862367@users.noreply.github.com>
2018-08-23 23:24:14 +1000

    Placed Items near Gridstone + Items only appear within radar
    - Placed 5 items around Gridstone and resized them to a suitable size
    - Location editor is now set to Gridstone
    - Items now only appear within radar (15m away from player)
    - Removed auto spawn mechanism and timed destroy item

---
+++ FinderKeeper/Assets/Scripts/TimeScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TimeScript : MonoBehaviour {
+    public Text TimeText;
+    public Text ScoreText;
+    public float MaxTime = 300;
+    public float SceneTime = 0;
+    public GameObject EndPanel;
+    private string TimeOutput = "REMAINING TIME:\n";
+
+
+	// Use this for initialization
+	void Start () {
+		EndPanel.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        SceneTime += Time.deltaTime;
+        if (SceneTime >= MaxTime)
+        {
+            EndGame.finalScore = ScoreText.text;
+            EndPanel.SetActive(true);
+        }
+        else
+        {
+            TimeText.text = TimeOutput + (Mathf.RoundToInt(MaxTime - SceneTime)).ToString();
+        }
+    }
+}
