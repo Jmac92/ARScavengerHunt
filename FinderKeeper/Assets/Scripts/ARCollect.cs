a3cdbec5e73b247db01af214606e9a7d312814fc
LMDyer <100593490@student.swin.edu.au>
2018-09-10 23:44:11 +1000

    Cubes disappear on collection
    Trying to add fun confetti

---
+++ FinderKeeper/Assets/Scripts/ARCollect.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARCollect : MonoBehaviour {
+
+    public ParticleSystem confetti;
+    private GameObject _collectedItem = null;
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit raycastHit;
+            if (Physics.Raycast(raycast, out raycastHit))
+            {
+                if (raycastHit.collider.tag == "Low")
+                {
+                    _collectedItem = raycastHit.transform.gameObject;
+                    CollectItem(_collectedItem);
+                }
+            }
+        }
+    }
+
+    private void CollectItem(GameObject item) {
+        item.GetComponent<MeshRenderer>().enabled = false;
+        confetti.Play();
+    }
+}
