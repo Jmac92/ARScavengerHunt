773820957cd1a6fbcede5d28bf6b229d60abc4c1
100862367 <38548638+100862367@users.noreply.github.com>
2018-10-06 02:53:20 +1000

    UI changes
    Added inventory list to prefab and removed from the scene
    Buttons are consistent for main menu, ready and end scene
    Added a help button in ready scene
    Rotation lock will show a lock padlock and unlock will show an unlock padlock
    Change icon color for overview map

---
+++ FinderKeeper/Assets/Scripts/Button.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Button : MonoBehaviour {
+    public Image rotationIcon;
+    public Sprite locked;
+    public Sprite unlocked;
+    private bool _unlock = true;
+
+	// Use this for initialization
+	void Start () {
+        rotationIcon.GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void Lock()
+    {
+        _unlock = !_unlock;
+        if (_unlock == false)
+        {
+            rotationIcon.overrideSprite = locked;
+        }
+        else
+        {
+            rotationIcon.overrideSprite = unlocked;
+        }
+    }
+}
